30c6dd6ae440ac9c438de413761fb6f626e6aa0c
PhanNgocTrieu <bettertogetheronit@gmail.com>
2021-08-03 10:36:41 +0700

    First Qt Project

---
+++ Qt_Project/HelloWorld/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btnSubmit_clicked()
+{
+    // Display Hello World to Label
+    ui->lblDisplay->setText("Hello from Client!");
+}
+
