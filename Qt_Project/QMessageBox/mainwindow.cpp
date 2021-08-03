76a610977b34d0ac22d843b8c2d1575cd02597f9
PhanNgocTrieu <bettertogetheronit@gmail.com>
2021-08-03 14:42:16 +0700

    QMessageBox Halding

---
+++ Qt_Project/QMessageBox/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btnMessage_clicked()
+{
+    QMessageBox::information(this,"infor","Hello every one, I'm John from Quang Nam, Viet Nam");
+}
+
